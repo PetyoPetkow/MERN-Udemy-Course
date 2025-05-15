6f5c9455ef4b33c432d91b1d40e9547653b30178
Petyo Petkov <Pspetkov13@gmail.com>
2025-05-16 00:45:12 +0300

    Link component

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,7 +1,13 @@
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login: FC<LoginProps> = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <h1>Login</h1>
+      <Link to="/register">Register</Link>
+    </div>
+  );
 };
 
 interface LoginProps {}
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,7 +1,13 @@
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 const Register: FC<RegisterProps> = () => {
-  return <div>Register</div>;
+  return (
+    <div>
+      <h1>Register</h1>
+      <Link to="/login">Login</Link>
+    </div>
+  );
 };
 
 interface RegisterProps {}
