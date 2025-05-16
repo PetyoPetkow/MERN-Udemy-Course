d61d0fea6eeac23383f50f09765f5bd40fdaf6e0
Petyo Petkov <Pspetkov13@gmail.com>
2025-05-16 16:16:58 +0300

    Error Page

--- client/src/pages/Error.tsx
+++ client/src/pages/Error.tsx
@@ -1,15 +1,32 @@
 import { FC } from 'react';
-import { Link, useRouteError } from 'react-router-dom';
+import { Link, useRouteError, isRouteErrorResponse } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/ErrorPage';
+import img from '../assets/images/not-found.svg';
 
 const Error: FC<ErrorProps> = () => {
   const error = useRouteError();
   console.log(error);
 
+  if (isRouteErrorResponse(error) && error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt="not found" />
+          <h3>Page not found</h3>
+          <p>We can't seem to find the page you are looking for</p>
+          <Link to="/dashboard">back home</Link>
+        </div>
+      </Wrapper>
+    );
+  }
+
   return (
-    <div>
-      <h1>Error Page</h1>
-      <Link to="/">Back home</Link>
-    </div>
+    <Wrapper>
+      <div>
+        <h3>Something went wrong</h3>
+        <Link to="/dashboard">back home</Link>
+      </div>
+    </Wrapper>
   );
 };
 
