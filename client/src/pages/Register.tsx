260fe56369d8caf87f4b6a4203bb2e3ddbc9f32a
Petyo Petkov <Pspetkov13@gmail.com>
2025-05-16 16:25:20 +0300

    Register Page

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,12 +1,36 @@
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+import { Logo } from '../components';
 
 const Register: FC<RegisterProps> = () => {
   return (
-    <div>
-      <h1>Register</h1>
-      <Link to="/login">Login</Link>
-    </div>
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>Register</h4>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="form-input"
+            defaultValue="john"
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+        <p>Already a member?</p>
+        <Link to="/login" className="member-btn">
+          Login
+        </Link>
+      </form>
+    </Wrapper>
   );
 };
 
