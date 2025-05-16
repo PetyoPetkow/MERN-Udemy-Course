5caec5aca1237edd693bd92274252c310a5bcd16
Petyo Petkov <Pspetkov13@gmail.com>
2025-05-16 15:51:15 +0300

    Landing page

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,10 +1,36 @@
 import { FC } from 'react';
+import Wrapper from '../assets/wrappers/LandingPage';
+import main from '../assets/images/main.svg';
+import logo from '../assets/images/logo.svg';
+import { Link } from 'react-router-dom';
 
 const Landing: FC<LandingProps> = () => {
   return (
-    <div>
-      <h1>Landing Page</h1>
-    </div>
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="jobify" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam natus
+            quod molestiae voluptas assumenda. Ipsam cum explicabo consequuntur
+            maiores voluptatum, eius possimus. Ratione aliquam excepturi quas
+            officiis autem alias aliquid.
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn">
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
   );
 };
 
