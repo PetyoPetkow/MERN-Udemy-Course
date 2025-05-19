eff1cbe730e1e933f8046d60d6f56d17d30b0a56
Petyo Petkov <Pspetkov13@gmail.com>
2025-05-19 16:27:22 +0300

    Sidebar Structure

--- client/src/components/SmallSidebar.tsx
+++ client/src/components/SmallSidebar.tsx
@@ -1,8 +1,36 @@
 import { FC } from 'react';
 import Wrapper from '../assets/wrappers/SmallSidebar';
+import { FaTimes } from 'react-icons/fa';
+import Logo from './Logo';
+import links from '../utils/links';
+import { NavLink } from 'react-router-dom';
+import { useDashboardContext } from '../pages/DashboardLayout';
 
 const SmallSidebar: FC<SmallSidebarProps> = () => {
-  return <Wrapper>SmallSidebar</Wrapper>;
+  const data = useDashboardContext();
+
+  return (
+    <Wrapper>
+      <div className="sidebar-container show-sidebar">
+        <div className="content">
+          <button type="button" className="close-btn">
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">
+            {links.map(({ text, path, icon }) => (
+              <NavLink to={path} key={text} className="nav-link">
+                <span className="icon">{icon}</span>
+                {text}
+              </NavLink>
+            ))}
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 interface SmallSidebarProps {}
