7090951b79183b7959b9a6f64bb8dd20efdf1db9
Petyo Petkov <Pspetkov13@gmail.com>
2025-05-19 17:03:17 +0300

    Big Sidebar

--- client/src/components/BigSidebar.tsx
+++ client/src/components/BigSidebar.tsx
@@ -1,8 +1,36 @@
 import { FC } from 'react';
 import Wrapper from '../assets/wrappers/BigSidebar';
+import { NavLink } from 'react-router-dom';
+import links from '../utils/links';
+import { useDashboardContext } from '../pages/DashboardLayout';
+import Logo from './Logo';
 
 const BigSidebar: FC<BigSidebarProps> = () => {
-  return <Wrapper>BigSidebar</Wrapper>;
+  const { showSidebar, toggleSidebar } = useDashboardContext();
+
+  return (
+    <Wrapper>
+      <div
+        className={
+          showSidebar ? 'sidebar-container' : 'sidebar-container show-sidebar'
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">
+            {links.map(({ text, path, icon }) => (
+              <NavLink to={path} key={text} className="nav-link" end>
+                <span className="icon">{icon}</span>
+                {text}
+              </NavLink>
+            ))}
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 interface BigSidebarProps {}
