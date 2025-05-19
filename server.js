87d5a621f4f677b1e66707b3767d609b63705103
Petyo Petkov <Pspetkov13@gmail.com>
2025-05-19 23:32:54 +0300

    Morgan and Dotenv

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
+import * as dotenv from 'dotenv'
+dotenv.config();
 import express from "express";
-const app = express()
+const app = express();
+import morgan from 'morgan'
 
-app.use(express.json())
+if(process.env.NODE_ENV === 'development'){
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
 
 app.get('/', (req, res)=>{
-  res.send('Hello World')
-})
+  res.send('Hello World');
+});
 
 app.post('/', (req,res)=>{
-  console.log(req)
-  res.json({message: 'data received', data: req.body})
-})
+  console.log(req);
+  res.json({message: 'data received', data: req.body});
+});
 
-app.listen(5100, ()=>{
-  console.log('server running...')
-})
+const port = process.env.PORT || 5100
+
+app.listen(port, ()=>{
+  console.log(`server running on PORT ${port}`);
+});
