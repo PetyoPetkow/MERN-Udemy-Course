394fa92bf8ca3189d8421b677390c63030b874ad
Petyo Petkov <Pspetkov13@gmail.com>
2025-05-20 00:40:36 +0300

    Get Job By Id

--- server.js
+++ server.js
@@ -16,37 +16,49 @@ if(process.env.NODE_ENV === 'development'){
 
 app.use(express.json());
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.post('/', (req, res)=>{
+app.post('/', (req, res) => {
   console.log(req);
   res.json({message: 'data received', data: req.body});
 });
 
 // GET ALL JOBS
-app.get('/api/v1/jobs', (req, res)=>{
+app.get('/api/v1/jobs', (req, res) => {
   res.status(200).json({ jobs });
 })
 
 // CREATE JOB
-app.post('/api/v1/jobs', (req, res)=>{
+app.post('/api/v1/jobs', (req, res) => {
   const {company, position} = req.body;
 
   if(!company || !position){
-    return res.status(400).json({ msg: 'please provide company and position' }) 
+    return res.status(400).json({ msg: 'please provide company and position' });
   }
 
   const id = nanoid(10);
   const job = {id, company, position};
   jobs.push(job);
 
-  res.status(200).json({ job });
+  res.status(201).json({ job });
+})
+
+// GET JOB BY ID
+app.get('/api/v1/jobs/:id', (req, res) => {
+  const {id} = req.params;
+
+  const job = jobs.find((job) => job.id === id)
+  if(!job){
+    return res.status(404).json({ msg: `no job with id ${id} was found` })
+  }
+
+  res.status(200).json({ job })
 })
 
 const port = process.env.PORT || 5100
 
-app.listen(port, ()=>{
+app.listen(port, () => {
   console.log(`server running on PORT ${port}`);
 });
