37c8b36d765378a7fb45e330efd0998862fdf772
Petyo Petkov <Pspetkov13@gmail.com>
2025-05-20 22:53:28 +0300

    Create Job Controller

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,3 +1,5 @@
+import JobModel from '../models/JobModel.js';
+
 import { nanoid } from 'nanoid';
 
 let jobs = [
@@ -10,16 +12,7 @@ export const getAllJobs = async (req, res) => {
 };
 
 export const createJob = async (req, res) => {
-  const {company, position} = req.body;
-
-  if(!company || !position){
-    return res.status(400).json({ msg: 'please provide company and position' });
-  }
-
-  const id = nanoid(10);
-  const job = {id, company, position};
-  jobs.push(job);
-
+  const job = await JobModel.create(req.body)
   res.status(201).json({ job });
 };
 
