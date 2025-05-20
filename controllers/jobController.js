9f1c7b9632652610f4e3eb65be3443ce3b4f7ad9
Petyo Petkov <Pspetkov13@gmail.com>
2025-05-20 23:33:25 +0300

    Status Codes

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,13 +1,14 @@
 import JobModel from '../models/JobModel.js';
+import StatusCodes from 'http-status-codes'
 
 export const getAllJobs = async (req, res) => {
   const jobs = await JobModel.find({})
-  res.status(200).json({ jobs });
+  res.status(StatusCodes.OK).json({ jobs });
 };
 
 export const createJob = async (req, res) => {
   const job = await JobModel.create(req.body)
-  res.status(201).json({ job });
+  res.status(StatusCodes.CREATED).json({ job });
 };
 
 export const getJobById = async (req, res) => {
@@ -15,10 +16,10 @@ export const getJobById = async (req, res) => {
 
   const job = await JobModel.findById(id)
   if(!job){
-    return res.status(404).json({ msg: `no job with id ${id} was found` });
+    return res.status(StatusCodes.NOT_FOUND).json({ msg: `no job with id ${id} was found` });
   }
 
-  res.status(200).json({ job });
+  res.status(StatusCodes.OK).json({ job });
 };
 
 export const updateJob = async (req, res) => {
@@ -29,10 +30,10 @@ export const updateJob = async (req, res) => {
   });
 
   if(!updatedJob){
-    return res.status(404).json({ msg: `no job with id ${id} was found` });
+    return res.status(StatusCodes.NOT_FOUND).json({ msg: `no job with id ${id} was found` });
   }
 
-  res.status(200).json({ msg: 'job edited sucessfully', job: updatedJob });
+  res.status(StatusCodes.OK).json({ msg: 'job edited sucessfully', job: updatedJob });
 };
 
 export const deleteJob = async (req, res) => {
@@ -40,8 +41,8 @@ export const deleteJob = async (req, res) => {
   const removedJob = await JobModel.findByIdAndDelete(id)
 
   if(!removedJob){
-    return res.status(404).json({ msg: `no job with id ${id} was found` });
+    return res.status(StatusCodes.NOT_FOUND).json({ msg: `no job with id ${id} was found` });
   }
 
-  res.status(200).json({ msg: `job deleted successfully`, job: removedJob });
+  res.status(StatusCodes.OK).json({ msg: `job deleted successfully`, job: removedJob });
 };
