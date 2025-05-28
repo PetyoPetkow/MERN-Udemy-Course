4bc6d12a6852ca45bdc86fcd30a1efd15e40e04f
Petyo Petkov <Pspetkov13@gmail.com>
2025-05-28 13:55:58 +0300

    Validate Job Input

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -3,8 +3,9 @@ import { Router } from 'express';
 const router = Router();
 
 import {getAllJobs,getJobById,createJob,updateJob,deleteJob} from '../controllers/jobController.js'
+import { validateJobInput } from '../middleware/validationMiddleware.js';
 
-router.route('/').get(getAllJobs).post(createJob);
-router.route('/:id').get(getJobById).patch(updateJob).delete(deleteJob);
+router.route('/').get(getAllJobs).post(validateJobInput, createJob);
+router.route('/:id').get(getJobById).patch(validateJobInput, updateJob).delete(deleteJob);
 
 export default router;
