f4c3dfaaa45dd023eb6c9fdbeb59ff4ff82d5ff1
Petyo Petkov <Pspetkov13@gmail.com>
2025-05-30 17:28:56 +0300

    JWT ENV

--- utils/tokenUtils.ts
+++ utils/tokenUtils.ts
@@ -1,7 +1,14 @@
 import jwt from 'jsonwebtoken';
 
 export const createJWT = (payload: string | Buffer | object) => {
-  const token = jwt.sign(payload, 'secret', { expiresIn: '1d' });
+  const secret = process.env.JWT_SECRET;
+  if (!secret) {
+    throw new Error('JWT_SECRET environment variable is not defined');
+  }
+
+  const token = jwt.sign(payload, secret, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   return token;
 };
