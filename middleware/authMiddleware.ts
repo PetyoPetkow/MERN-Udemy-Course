ded74d2936d6c3253d95d082c0e1f72b750ed876
Petyo Petkov <Pspetkov13@gmail.com>
2025-06-04 11:37:45 +0300

    Verify Cookie

--- middleware/authMiddleware.ts
+++ middleware/authMiddleware.ts
@@ -1,4 +1,10 @@
-export const authenticateUser = async (req, res, next) => {
-  console.log('auth middleware');
+import { Middleware } from 'express-validator/src/base';
+import { UnauthenticatedError } from '../errors/customErrors';
+
+export const authenticateUser: Middleware = async (req, res, next) => {
+  const token = req.cookies?.token;
+
+  if (!token) throw new UnauthenticatedError('authentication invalid');
+
   next();
 };
